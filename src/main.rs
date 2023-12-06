bb89f42f8146eda9f850ffb592ea7084056ccc23
WarLockeR31 <roman.borisenko2016@gmail.com>
2023-12-06 22:14:46 +0300

    Add Dockerfile and UnitTests

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     const N: usize = 200;
     let mut x = vec![0; N];
 
+    println!("Array:");
     for i in 0..N {
         x[i] = N as i32 - i as i32;
         print!("{:4}", x[i]);
@@ -44,4 +45,36 @@ fn partition(x: &mut Vec<i32>, low: usize, high: usize) -> usize {
 
     x.swap(i, high);
     i
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    //use super::*;
+
+    use crate::my_sort;
+
+    #[test]
+    fn test1() {
+        let mut v1 = vec![1, 0, 3, 2];
+        let v2 = vec![0, 1, 2, 3];
+        my_sort(&mut v1);
+        assert_eq!(v1, v2);
+    }
+
+    #[test]
+    fn test2() {
+        let mut v1 = vec![0, 0, 0, -1];
+        let v2 = vec![-1, 0, 0, 0];
+        my_sort(&mut v1);
+        assert_eq!(v1, v2);
+    }
+
+    #[test]
+    fn test3() {
+        let mut v1 = vec![1, 1, 1, 1];
+        let v2 = vec![1, 1, 1, 1];
+        my_sort(&mut v1);
+        assert_eq!(v1, v2);
+    }
 }
